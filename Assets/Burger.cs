2a3f36b1a4ba34065bb682416cf12b30b035fe1c
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2023-12-07 18:58:54 +0200

    Initial commit

---
+++ Assets/Burger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burger : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GameManager.clicks++;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
