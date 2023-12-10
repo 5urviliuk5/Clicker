4a732f2bba120bccde5c689d7033adb9a06f9604
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2023-12-10 16:58:51 +0200

    Player Prefs

--- Assets/Game Manager.cs
+++ Assets/Game Manager.cs
@@ -8,8 +8,29 @@ public class GameManager : MonoBehaviour
     public static int clicks;
     public TMP_Text clicksText;
 
-    private void Update()
+    private void Start()
+    {
+        Load();
+        InvokeRepeating("Save", 3f,3f);
+    }
+
+    void Update()
     {
         clicksText.text = clicks.ToString();
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("clicks", clicks);
+    }
+
+    void Load()
+    {
+        clicks = PlayerPrefs.GetInt("clicks");
+    }
+
+    void OnApplicationQuit()
+    {
+        Save();
     }
 }
