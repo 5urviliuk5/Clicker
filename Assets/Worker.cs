d4610a9188ecb89733411cb8b086b54295b4503d
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2023-12-10 17:15:27 +0200

    Worker script

--- Assets/Worker.cs
+++ Assets/Worker.cs
@@ -6,9 +6,40 @@ public class Worker : MonoBehaviour
 {
     public int count;
     public int cost;
+    public int cps = 1;
+    public string name;
+
+    private void Start()
+    {
+        InvokeRepeating("Work", 1f, 1f);
+        Load();
+    }
 
     public void Buy()
     {
+        if (GameManager.clicks < cost) return;
+
         count++;
+        GameManager.clicks -= cost;
+        cost = (int)(cost * 1.3f);
+
+        Save();
+    }
+
+    void Work()
+    {
+        GameManager.clicks += count * cps;
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt(name + "count",count);
+        PlayerPrefs.SetInt(name + "cost", cost);
+    }
+    
+    void Load()
+    {
+        count = PlayerPrefs.GetInt(name + "count");
+        cost = PlayerPrefs.GetInt(name + "cost",cost);
     }
 }
